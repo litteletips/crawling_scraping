11a3b17099f29ace338b8365df2247a97543113c
上山　俊佑 <>
2019-12-31 14:58:39 +0900

    mechanicalSoupでcookpadレシピ取得

--- crawling_scraping/open_data_extraction/get_museums.py
+++ crawling_scraping/open_data_extraction/get_museums.py
@@ -1,3 +1,20 @@
+# Linked Open Dataからのデータ収集。
+# データ同士をリンクさせ、容易に検索できる形で公開する為の方法論をLinked Dataと呼ぶ。
+
+# Linked Dataにおけるデータ同士のリンクは、RDFというデータモデルで記述される。
+# データを主語、述語、目的語の3組でモデル化。(日本(主語)、首都(述語)、東京(目的語))
+# それぞれをURIで定義する。
+
+# RDFのデータベースからデータを検索するために、SPARQLというクエリ言語が使える。
+# クエリだけでなく、HTTPを使った通信の為のプロトコルも定義されており、SPARQLエンドポイントと呼ばれるサーバーにクエリを送信して、結果を取得できる。
+# SPARQLによって、サーバーごとに異なるWebAPIの使い方を覚えることなく、統一されたインターフェイスでデータ取得できる。
+
+# Wikipedia日本語版から情報を抽出して、Linked Open Dataとして提供するプロジェクト(DBpedia Japanese)
+# DBpedia JapaneseからSPARQLで日本の美術館の情報を収集する。
+
+# 実行方法
+# python print_pdf_textboxes.py　000232384.pdf
+
 from SPARQLWrapper import SPARQLWrapper  # pip install SPARQLWrapper
 
 # SPARQLエンドポイントのURLを指定してインスタンスを作成する。
